143f05a7b10a6c7e58b9ac7c73aae64b14aa4481
Jayson Reis <santosdosreis@gmail.com>
2023-07-05 07:01:00 +0200

    Rewrite pixman to use meson for the builds

--- gvsbuild/projects/pixman.py
+++ gvsbuild/projects/pixman.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import os
+from gvsbuild.utils.base_builders import Meson
 
 from gvsbuild.utils.base_expanders import Tarball
 from gvsbuild.utils.base_project import Project, project_add
@@ -35,20 +35,10 @@ class Pixman(Tarball, Project):
         )
 
     def build(self):
-        optimizations = "SSE2=on SSSE3=on" + (
-            " MMX=off" if self.builder.x64 else " MMX=on"
+        enable_mmx = "off" if self.builder.x64 else "on"
+        Meson.build(
+            self,
+            meson_params=f"-Dsse2=on -Dssse3=on -Dmmx={enable_mmx} -Dgtk=off -Dlibpng=off -Dtests=off -Ddemo=off",
         )
-
-        add_path = os.path.join(self.builder.opts.msys_dir, "usr", "bin")
-
-        self.exec_vs(
-            f"make -f Makefile.win32 pixman CFG=%(configuration)s {optimizations}",
-            add_path=add_path,
-        )
-
-        self.install(r".\pixman\%(configuration)s\pixman-1.lib lib")
-
-        self.install(r".\pixman\pixman.h include\pixman-1")
-        self.install(r".\pixman\pixman-version.h include\pixman-1")
 
         self.install(r".\COPYING share\doc\pixman")
